37e729e5c935ffe7a178e12b9a7ddbc49c489f00
mindgitrwx <hybridkernal@gmail.com>
2020-10-28 19:32:08 +0900

    mod - general color

--- app/customization/styleVariables.js
+++ app/customization/styleVariables.js
@@ -4,7 +4,7 @@ export const background = 'url("http://s3.amazonaws.com/mindgitrwx/assets/img/bg
 // =================================COLORS=================================
 
 // --- Char General Colors ---//
-export const primaryColor    = '#dc2a23';
+export const primaryColor    = '#0000ff';
 export const onlineColor     = '#7ed321';
 export const offlineColor    = '#e5e5e5';
 export const textColor       = '#5d6975';
