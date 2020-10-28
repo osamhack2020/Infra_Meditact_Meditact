dc971d2578ae33ef0bb5be8dfd971ad989965e16
mindgitrwx <hybridkernal@gmail.com>
2020-10-28 23:23:08 +0900

    mod - case issue to remove error

--- app/BotMind/BotMindFlows/index.js
+++ app/BotMind/BotMindFlows/index.js
@@ -157,7 +157,7 @@ const questions                 = {
       },
       {
         answer   : 'AI정보',
-        nextId   : 'aIInfo',
+        nextId   : 'aiInfo',
         sumToBags: [{ name: 'rickAndMorty', points: 3 }, { name: 'shroedingersCat', points: 2 }, { name: 'recursion', points: 1 }],
       },
       {
@@ -181,10 +181,10 @@ const questions                 = {
 			{ nextId: 'select' },
     ],
   },
-  aIinfo: {
+  aiInfo: {
     botPrompt: "현재 다양한 모델을 적용시킬 예정입니다. AI를 개선해주고 싶은 분이 있으면 여기에다 issue를 달아주세요.",
     answers  : [
-			{ nextId: 'aIinfoScale' },
+			{ nextId: 'aiInfoScale' },
     ],
   },
   teamInfo: {
@@ -193,7 +193,7 @@ const questions                 = {
 			{ nextId: 'select' },
     ],
   },
-  aIinfoScale: {
+  aiInfoScale: {
     botPrompt: "의학 분야 전문 지식을 가진 군의관님이 11만 8008개의 데이터를 크롤링하고 라벨링해서 만들어졌어요!",
     answers  : [
 			{ nextId: 'aIinfoLink' },
